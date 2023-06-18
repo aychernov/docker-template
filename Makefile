75e2aa7633427d9ea27b58664fd1182750f17a72
Aleksandr Chernov <sashacool75@gmail.com>
2023-06-18 15:28:33 +0300

    auto restart template

--- Makefile
+++ Makefile
@@ -1,2 +1,28 @@
 mongo:
-	docker run -d -p 27017:27017 --rm --name mongodb mongo
+	docker run -p 27017:27017 \
+		-d \
+		--rm --name mongodb \
+		--network notes-net \
+		--env-file ./config/development.env \
+		-v mongo-data:/data/db \
+		mongo
+
+backend:
+	docker run -p 5000:5000 \
+		--rm \
+		--name notes-backend \
+		--network notes-net \
+		-v D:\SHAREWOOD\docker-practicum-vladilen\server:/app \
+		-v /app/node_modules \
+		--env-file ./config/development.env \
+		notes-backend
+
+frontend:
+	docker run -p 3000:3000 \
+		-d \
+		--rm \
+		--name notes-frontend \
+		--network notes-net \
+		-e CHOKIDAR_USEPOLLING=true \
+		-v D:\SHAREWOOD\docker-practicum-vladilen\client\src:/app/src \
+		notes-frontend
--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 const routes = require('./routes/note.routes')
 
 const app = express()
 const port = process.env.PORT ?? 5000
 
+app.use(cors())
 app.use(express.json({ extended: true }))
 app.use('/api/note', routes)
 
@@ -19,7 +21,13 @@ if (process.env.NODE_ENV === 'production') {
 
 async function start() {
   try {
-    await mongoose.connect(`mongodb://localhost:27017/notes`, {
+    const {
+      MONGO_INITDB_ROOT_USERNAME: username,
+      MONGO_INITDB_ROOT_PASSWORD: password,
+        MONGO_HOST: host
+    } = process.env
+    const uri = `mongodb://${username}:${password}@${host}/notes?authSource=admin`
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
